31733ebcfc1d0a159c3b764662e362a75132efe3
peimanhp <peiman_hp@yahoo.com>
2023-02-22 01:44:26 +0330

    restart game added

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
-const gameBoard = {};
+let gameBoard = {};
 const magicSquare = [8, 1, 6, 3, 5, 7, 4, 9, 2];
-const x = [];
-const o = [];
+let x = [];
+let o = [];
 const result = document.querySelector(".result");
+const restartBtn = document.querySelector(".restart");
 function Player(name) {
   this.name = name;
 }
@@ -10,10 +11,13 @@ const playerX = new Player("x");
 const playerO = new Player("o");
 
 const cells = document.querySelectorAll(".cells");
-for (let i = 0; i < cells.length; i++) {
-  cells[i].addEventListener("click", checkSign);
-  gameBoard[cells[i].id] = cells[i].innerHTML;
+function cellsAddEvents() {
+  for (let i = 0; i < cells.length; i++) {
+    cells[i].addEventListener("click", checkSign);
+    gameBoard[cells[i].id] = cells[i].innerHTML;
+  }
 }
+cellsAddEvents();
 console.log(gameBoard);
 
 let clickCounter = 0;
@@ -33,16 +37,13 @@ function checkSign(e) {
   }
   e.target.removeEventListener("click", checkSign);
 }
-let gameOver = false;
 
 function winCheck() {
   if (x.length < 3) return;
   else if (x.length == 3) {
     const sum = x.reduce((acc, cur) => acc + cur);
-    if (sum == 15) {      
-      result.innerHTML = "Player X won!";
-      gameOver = true;
-      console.log("win");
+    if (sum == 15) {
+      playerWin();
     }
   } else if (x.length == 4) {
     for (let i = 0; i < x.length; i++) {
@@ -55,9 +56,7 @@ function winCheck() {
       }
       console.log(sum);
       if (sum == 15) {
-        console.log("win");
-        result.innerHTML = "Player X won!";
-        gameOver = true;
+        playerWin();
         break;
       }
     }
@@ -72,11 +71,37 @@ function winCheck() {
       }
       console.log(sum);
       if (sum == 15) {
-        console.log("win");
-        result.innerHTML = "Player X won!";        
-        gameOver = true;
+        playerWin();
         break;
       }
     }
   }
+}
+
+function playerWin() {
+  console.log("win");
+  result.innerHTML = "Player X won!";
+  restartBtn.classList.add('show');
+  stopPlaying();
+}
+
+function stopPlaying() {
+  for (const cell of cells) {
+    cell.removeEventListener("click", checkSign);
+  }
+}
+
+restartBtn.addEventListener('click', resetGame);
+
+function resetGame() {
+  gameBoard = [];
+  x = [];
+  o = [];
+  result.innerHTML = '';  
+  restartBtn.classList.remove('show');
+  for (const cell of cells) {
+    cell.innerHTML = '';
+  }
+  cellsAddEvents();
+  clickCounter = 0;
 }
